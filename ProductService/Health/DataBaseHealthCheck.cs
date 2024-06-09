0e25e8c5e2f2d9a4a6c61763d9a7d3065a5d98fb
amirbaa1 <amir.baahklag@yahoo.com>
2024-06-09 17:33:33 +0330

    update

--- ProductService/Health/DataBaseHealthCheck.cs
+++ ProductService/Health/DataBaseHealthCheck.cs
@@ -5,27 +5,6 @@ namespace ProductService.Health;
 
 public class DataBaseHealthCheck : IHealthCheck
 {
-    // public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
-    //     CancellationToken cancellationToken = new CancellationToken())
-    // {
-    //     using (var connect =
-    //            new NpgsqlConnection("Host=localhost;Port=5432;Database=Productdb;Username=postgres;Password=amir$$1379"))
-    //     {
-    //         try
-    //         {
-    //             await connect.OpenAsync(cancellationToken);
-    //             var command = connect.CreateCommand();
-    //             command.CommandText = "select 1";
-    //             await command.ExecuteNonQueryAsync(cancellationToken);
-    //         }
-    //         catch (Exception e)
-    //         {
-    //             return new HealthCheckResult(status: context.Registration.FailureStatus, exception: e);
-    //         }
-    //     }
-    //
-    //     return HealthCheckResult.Healthy();
-    // }
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
     {
         throw new NotImplementedException();
