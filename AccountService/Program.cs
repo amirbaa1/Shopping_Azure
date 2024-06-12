178c648b48388c781445de50821c307f56489ca4
amirbaa1 <amir.baahklag@yahoo.com>
2024-06-12 14:21:36 +0330

    update identity Account

---
+++ AccountService/Program.cs
@@ -0,0 +1,74 @@
+using System.Text;
+using AccountService.Data;
+using AccountService.Model;
+using AccountService.Model.Dto;
+using AccountService.Services;
+using AccountService.Services.Token;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//-----------------Data----------------//
+builder.Services.AddDbContext<AccountDbContext>(x =>
+    x.UseNpgsql(builder.Configuration["ConnectionStrings:AccountConnectionString"]));
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(
+        op => { op.SignIn.RequireConfirmedEmail = true; })
+    .AddEntityFrameworkStores<AccountDbContext>()
+    .AddDefaultTokenProviders();
+//-------------------------------------//
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+builder.Services.AddSingleton<ResponseDto>();
+builder.Services.Configure<JwtOption>(builder.Configuration.GetSection("TokenAuthAPI:JWTOption"));
+
+// ----------------- JWT -------------------//
+builder.Services.AddAuthentication(op =>
+{
+    op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(op =>
+{
+    op.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey =
+            new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("TokenAuthAPI:JWTOption:Secret")!)),
+        ValidateLifetime = true,
+        ValidateAudience = false,
+        ValidateIssuer = true,
+        ClockSkew = TimeSpan.Zero,
+    };
+});
+//--------------------------------------//
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
