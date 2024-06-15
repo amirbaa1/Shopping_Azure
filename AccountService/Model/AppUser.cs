c56aba4447b1159278944b506470664bcfae4438
AMIR_bA <53426863+amirbaa1@users.noreply.github.com>
2024-06-15 17:21:36 +0330

    update add profile model update appuser add profile service

--- AccountService/Model/AppUser.cs
+++ AccountService/Model/AppUser.cs
@@ -4,5 +4,7 @@ namespace AccountService.Model;
 
 public class AppUser : IdentityUser
 {
-    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? Image { get; set; }
 }
--- AccountService/Services/IAuthService.cs
+++ AccountService/Services/IAuthService.cs
@@ -1,5 +1,6 @@
 using AccountService.Model;
 using AccountService.Model.Dto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AccountService.Services;
 
@@ -8,4 +9,8 @@ public interface IAuthService
     Task<string> Register(RegisterModel register);
 
     Task<LoginResponseDto> Login(LoginModel login);
+
+    Task<ResponseDto> ProfileService(string Id);
+
+    Task<ResponseDto> UpdateProfile(string id,UpdateProfile updateProfiles);
 }
